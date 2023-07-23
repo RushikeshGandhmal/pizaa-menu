f59119140b9fb86a16da12ff647e9369bf1b0cb0
Rushi <rushikeshgandhmal@gmail.com>
2023-07-23 18:14:36 +0530

    added imgs and data

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom/client";
 
 const App = () => <h1>Hello React!</h1>;
 
+const pizaa = () => {
+  <div>
+    <img src="./piz" alt="" />
+    <h1></h1>
+    <p></p>
+  </div>;
+};
+
 // React v18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
